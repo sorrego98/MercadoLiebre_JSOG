e0613d79d3bf698519f7adff528fd36d946c1ad7
sorrego98 <juan.sorrego98@gmail.com>
2023-06-11 16:35:45 -0500

    Agregado ruta public

--- src/app.js
+++ src/app.js
@@ -3,5 +3,8 @@ const app = express();
 const path = require(`path`);
 
 const port = 3030;
+const publicPath = path.resolve(__dirname, `../public`);
+
+app.use
 
 app.listen(port, () => console.log(`Server done`));
