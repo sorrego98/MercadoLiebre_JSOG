6837f3e465ce660a11cf47ff3de917fb842e3efd
sorrego98 <juan.sorrego98@gmail.com>
2023-06-12 10:32:15 -0500

    Creación de la estructura del WebSite, y agregado de los productos ofertados

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ const path = require(`path`);
 
 const port = 3030;
 const publicPath = path.resolve(__dirname, `../public`);
+const homePath = path.resolve(__dirname, `./views/home.html`)
 
-app.use
+app.get(`/home`, (req, res) => res.sendFile(homePath));
+app.use(express.static(publicPath));
 
 app.listen(port, () => console.log(`Server done`));
