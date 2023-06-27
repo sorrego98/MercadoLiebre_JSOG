c3c4d72655874eedce5e103dd888d99a2f9e8931
sorrego98 <juan.sorrego98@gmail.com>
2023-06-26 20:54:57 -0500

    registro y login

--- src/app.js
+++ src/app.js
@@ -6,9 +6,12 @@ const port = 3030;
 const publicPath = path.resolve(__dirname, `../public`);
 const homePath = path.resolve(__dirname, `./views/home.html`)
 const registerPath = path.resolve(__dirname, `./views/register.html`)
+const loginPath = path.resolve(__dirname, `./views/login.html`)
+app.use(express.static(publicPath));
 
 app.get(`/home`, (req, res) => res.sendFile(homePath));
 app.get(`/register`, (req, res) => res.sendFile(registerPath));
-app.use(express.static(publicPath));
+app.get(`/login`, (req, res) => res.sendFile(loginPath));
+
 
 app.listen(port, () => console.log(`Server done`));
