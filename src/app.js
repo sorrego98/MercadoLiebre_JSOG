a2dfff61dc05f4a5bcf6293468624cea3e747b08
sorrego98 <juan.sorrego98@gmail.com>
2023-07-18 11:34:05 -0500

    cambios sugeridos en la peer review

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require(`express`);
 const app = express();
 const path = require(`path`);
 
-const port = process.env.PORT || 3001;
+const port = 3030;
 const publicPath = path.resolve(__dirname, `../public`);
 const homePath = path.resolve(__dirname, `./views/home.html`)
 const registerPath = path.resolve(__dirname, `./views/register.html`)
